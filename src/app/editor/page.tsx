c0536414b230d9b8db50d726ff2affbcb2a6f6d9
Jay Prakash <jay.prakash@ksolves.com>
2026-01-12 08:13:42 +0000

    Try fixing this error: `Runtime TypeError: setTheme is not a function. E

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,5 +1,13 @@
+
+'use client';
+
 import { IdeLayout } from '@/components/versacode/ide-layout';
 
-export default function EditorPage() {
-  return <IdeLayout />;
+interface EditorPageProps {
+  theme: string;
+  setTheme: (theme: string) => void;
+}
+
+export default function EditorPage({ theme, setTheme }: EditorPageProps) {
+  return <IdeLayout theme={theme} setTheme={setTheme} />;
 }
